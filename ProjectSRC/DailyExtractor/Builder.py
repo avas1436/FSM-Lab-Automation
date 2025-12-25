3cce17b951350cf53f9e00e46c00c25b62620031
abAsz <abas.zade@outlook.com>
2025-12-25 18:37:47 +0330

    add pandas to adapter and intialize first features of labresultbuilder

--- ProjectSRC/DailyExtractor/Builder.py
+++ ProjectSRC/DailyExtractor/Builder.py
@@ -1,8 +1,12 @@
 from abc import ABC
+from decimal import Decimal
 
+import pandas as pd  # type: ignore
 from openpyxl import load_workbook  # type: ignore
+from Product import LabResult  # type: ignore
 
 
+# Interface
 class ExcelAdapter(ABC):
     """یک کلاس پایه که همه ادپتر های زیر مجموعه باید از آن ارث ببرند"""
 
@@ -10,10 +14,11 @@ class ExcelAdapter(ABC):
         raise NotImplementedError
 
 
+# Adapter with openpyxl
 class Openpyxl(ExcelAdapter):
     """لود کردن دیتا در رم به صورت یکجا"""
 
-    def __init__(self, file_path: str = "daily.xlsx", start: int = 1, end: int = 31):
+    def __init__(self, file_path: str, start: int, end: int):
         self.file_path = file_path
         self.start = start
         self.end = end
@@ -33,13 +38,60 @@ class Openpyxl(ExcelAdapter):
         wb.close()
 
 
+# Adapter with pandas
 class Pandas(ExcelAdapter):
+
+    def __init__(self, file_path: str, start: int, end: int):
+        self.file_path = file_path
+        self.start = start
+        self.end = end
+
     def get_records(self):
-        return super().get_records()
+        # خواندن کل فایل اکسل
+        xls = pd.ExcelFile(self.file_path)
+        sheet_names = xls.sheet_names
+        active_sheets = sheet_names[self.start - 1 : self.end]
+        for sheet in active_sheets:
+            # خواندن محدوده‌ی سلول‌ها (A4:L31) # skiprows=3 یعنی از ردیف چهارم شروع کند (چون اندیس از 0 است)
+            df = pd.read_excel(
+                self.file_path,
+                sheet_name=sheet,
+                header=None,
+                skiprows=3,  # شروع از ردیف 4
+                nrows=28,  # تا ردیف 31 (31-4+1 = 28 ردیف)
+                usecols="A:L",  # ستون‌های A تا L
+            )
+
+        # تبدیل به لیست لیست‌ها مثل خروجی openpyxl
+        sheet_data = df.values.tolist()
+
+        yield sheet_data
+
+
+# Facade
+class ExcelAdapterFacade:
+    def __init__(self, file_path: str, start: int = 1, end: int = 31, engine: str = "openpyxl"):
+        if engine == "openpyxl":
+            self.adapter = Openpyxl(file_path=file_path, start=start, end=end)
+        elif engine == "pandas":
+            self.adapter = Pandas(file_path=file_path, start=start, end=end)  # type: ignore
+        else:
+            raise ValueError("Engine must be 'pandas' or 'openpyxl'")
+
+    def get_records(self):
+        return self.adapter.get_records()
 
 
 class LabResultBuilder:
-    pass
+    def __init__(self, raw_data: list[list]):
+        self.raw_data = raw_data
+        self._records = {}
+
+    def parse(self):
+        pass
+
+    def build(self) -> LabResult:
+        return LabResult(**self._data)
 
 
 # openpyxl adaptor test:
