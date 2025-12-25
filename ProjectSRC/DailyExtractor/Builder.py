177d0a3cad98a534b931d21d286fb9b112d9ed7b
abAsz <abas.zade@outlook.com>
2025-12-25 12:58:43 +0330

    feat: initialize default parameters in openpyxl class

--- ProjectSRC/DailyExtractor/Builder.py
+++ ProjectSRC/DailyExtractor/Builder.py
@@ -11,8 +11,15 @@ class ExcelAdapter(ABC):
 
 
 class Openpyxl(ExcelAdapter):
+    """لود کردن دیتا در رم به صورت یکجا"""
+
+    def __init__(self, file_path: str = "daily.xlsx", start: int = 1, end: int = 31):
+        self.file_path = file_path
+        self.start = start
+        self.end = end
+
     def get_records(self):
-        return super().get_records()
+        pass
 
 
 class Pandas(ExcelAdapter):
