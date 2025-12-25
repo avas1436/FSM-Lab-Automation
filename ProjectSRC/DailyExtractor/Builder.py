e48a3ef7de15f4b5d8c7ba7514109b6dfeb9cab7
abAsz <abas.zade@outlook.com>
2025-12-25 12:44:50 +0330

    feat: initialize Builder in Adapter pattern.

--- ProjectSRC/DailyExtractor/Builder.py
+++ ProjectSRC/DailyExtractor/Builder.py
@@ -1,2 +1,24 @@
+from abc import ABC
+
+from openpyxl import load_workbook
+
+
+class ExcelAdapter(ABC):
+    """یک کلاس پایه که همه ادپتر های زیر مجموعه باید از آن ارث ببرند"""
+
+    def get_records(self):
+        raise NotImplementedError
+
+
+class Openpyxl(ExcelAdapter):
+    def get_records(self):
+        return super().get_records()
+
+
+class Pandas(ExcelAdapter):
+    def get_records(self):
+        return super().get_records()
+
+
 class LabResultBuilder:
     pass
