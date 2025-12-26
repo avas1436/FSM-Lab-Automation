913206aec0f89e56345462cba52e37dc66f55eef
abAsz <abas.zade@outlook.com>
2025-12-26 18:04:10 +0330

    feat: first use of builder model there is a lots of bug still: - save all of data in just one cell of excel - there isnt any header for file

--- ProjectSRC/DailyExtractor/Director.py
+++ ProjectSRC/DailyExtractor/Director.py
@@ -1,8 +1,12 @@
 import csv
+import os
 import sqlite3
 from abc import ABC, abstractmethod
+from decimal import Decimal
 
 import toml  # type: ignore
+from genericpath import exists
+from Product import LabResult  # type: ignore
 
 # Srategy pattern
 
@@ -20,12 +24,15 @@ class CsvSaver(Saver):
         self.file_path = file_path
 
     def __enter__(self):
+
+        os.makedirs(os.path.dirname(self.file_path), exist_ok=True)
+
         self.out_file = open(file=self.file_path, mode="w", newline="", encoding="utf-8")
         self.csv_writer = csv.writer(self.out_file, delimiter="|")
         return self
 
     def save(self, data):
-        self.csv_writer.writerow(data.dict().values())
+        self.csv_writer.writerow(data.model_dump().values())
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
@@ -46,3 +53,25 @@ class SqliteSaver(Saver):
 # Director - from Builder Pattern
 class LabResultManager:
     pass
+
+
+data = LabResult(
+    sampleID="5cdd6790-e061-4009-8f27-2e90b3efb0be",
+    year="1404",
+    month="07",
+    day="01",
+    time="0800",
+    timestamp=1758601800,
+    klin1=Decimal("4.72"),
+    klin2=Decimal("3.36"),
+    above40=Decimal("2.35"),
+    par05=Decimal("3.9"),
+    par51=Decimal("3.7"),
+    par10=Decimal("7.6"),
+    par16=Decimal("92.4"),
+    par60=Decimal("0.0"),
+)
+
+
+with CsvSaver() as saver:
+    saver.save(data=data)
