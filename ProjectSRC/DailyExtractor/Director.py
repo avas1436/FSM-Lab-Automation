d61ba7d0ca6763736fd8c4fcffb0ecf07be35ab2
abAsz <abas.zade@outlook.com>
2025-12-26 13:15:02 +0330

    feat(builder): initialize Director with Strategy for multi-method data saving

--- ProjectSRC/DailyExtractor/Director.py
+++ ProjectSRC/DailyExtractor/Director.py
@@ -1,2 +1,35 @@
+import csv
+import sqlite3
+from abc import ABC, abstractmethod
+
+import toml  # type: ignore
+
+# Srategy pattern
+
+
+# --- Strategy Interface ---
+class Saver(ABC):
+    @abstractmethod
+    def save(self, data):
+        pass
+
+
+# --- Concrete Strategies ---
+class CsvSaver(Saver):
+    def save(self, data):
+        return super().save(data)
+
+
+class TomlSaver(Saver):
+    def save(self, data):
+        return super().save(data)
+
+
+class SqliteSaver(Saver):
+    def save(self, data):
+        return super().save(data)
+
+
+# Director - from Builder Pattern
 class LabResultManager:
     pass
