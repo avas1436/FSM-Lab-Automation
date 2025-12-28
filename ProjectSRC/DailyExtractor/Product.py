5c00659ee1294ca84d7bb20dce0cfbc6c0d6c28d
avAs <abas.zade@outlook.com>
2025-12-28 15:26:29 +0330

    refactor: comment all of Product.py loggers

--- ProjectSRC/DailyExtractor/Product.py
+++ ProjectSRC/DailyExtractor/Product.py
@@ -4,7 +4,7 @@ from decimal import ROUND_HALF_UP, Decimal, InvalidOperation
 import jdatetime  # type: ignore
 from pydantic import BaseModel, Field, field_validator
 
-from ProjectSRC.Logger.logger_config import logger  # type: ignore
+# from ProjectSRC.Logger.logger_config import logger  # type: ignore
 
 
 class LabResult(BaseModel):
@@ -25,63 +25,65 @@ class LabResult(BaseModel):
     @field_validator("year", mode="before")
     def check_year(cls, v):
         if not v.isdigit():
-            logger.warning(f"[LabResult] [Field:year] [Status:invalid] Non-numeric value: {v}")
+            # logger.warning(f"[LabResult] [Field:year] [Status:invalid] Non-numeric value: {v}")
             raise ValueError("Year must be numeric")
 
         year_int = int(v)
         if not (1400 <= year_int):
-            logger.warning(f"[LabResult] [Field:year] [Status:invalid] Out of range (<1400): {v}")
+            # logger.warning(f"[LabResult] [Field:year] [Status:invalid] Out of range (<1400): {v}")
             raise ValueError("Year must be between 1400 and above")
-        logger.debug(f"[LabResult] [Field:year] [Status:valid] Value={v}")
+        # logger.debug(f"[LabResult] [Field:year] [Status:valid] Value={v}")
         return v
 
     @field_validator("month", mode="before")
     def check_month_range(cls, v):
         if not v.isdigit():
-            logger.warning(f"[LabResult] [Field:month] [Status:invalid] Non-numeric value: {v}")
+            # logger.warning(f"[LabResult] [Field:month] [Status:invalid] Non-numeric value: {v}")
             raise ValueError("Month must be numeric")
         month_int = int(v)
         if not (1 <= month_int <= 12):
-            logger.warning(f"[LabResult] [Field:month] [Status:invalid] Out of range (01-12): {v}")
+            # logger.warning(f"[LabResult] [Field:month] [Status:invalid] Out of range (01-12): {v}")
             raise ValueError("Month must be between 01 and 12")
         return v
 
     @field_validator("day", mode="before")
     def check_day_range(cls, v):
         if not v.isdigit():
-            logger.warning(f"[LabResult] [Field:day] [Status:invalid] Non-numeric value: {v}")
+            # logger.warning(f"[LabResult] [Field:day] [Status:invalid] Non-numeric value: {v}")
             raise ValueError("Day must be numeric")
         day_int = int(v)
         if not (1 <= day_int <= 31):
-            logger.warning(f"[LabResult] [Field:day] [Status:invalid] Out of range (01-31): {v}")
+            # logger.warning(f"[LabResult] [Field:day] [Status:invalid] Out of range (01-31): {v}")
             raise ValueError("Day must be between 01 and 31")
-        logger.debug(f"[LabResult] [Field:day] [Status:valid] Value={v}")
+        # logger.debug(f"[LabResult] [Field:day] [Status:valid] Value={v}")
         return v
 
     @field_validator("klin1", "klin2", "above40", mode="before")
     def safe_decimal_co2(value) -> str:
         try:
-            round = Decimal(str(value)).quantize(Decimal("0.01"), rounding=ROUND_HALF_UP)
-            logger.debug(f"[LabResult] [Field:CO2] [Status:valid] Value={round}")
+            round = Decimal(str(value)).quantize(
+                Decimal("0.01"), rounding=ROUND_HALF_UP
+            )
+            # logger.debug(f"[LabResult] [Field:CO2] [Status:valid] Value={round}")
             return str(round)
         except (InvalidOperation, ValueError):
-            logger.warning(f"[LabResult] [Field:CO2] [Status:invalid] Value={value}")
+            # logger.warning(f"[LabResult] [Field:CO2] [Status:invalid] Value={value}")
             return "Not Tested"
 
     @field_validator("par05", "par51", "par60", mode="before")
     def safe_decimal_par(value) -> str:
         try:
             round = Decimal(str(value)).quantize(Decimal("0.1"), rounding=ROUND_HALF_UP)
-            logger.debug(f"[LabResult] [Field:PAR] [Status:valid] Value={round}")
+            # logger.debug(f"[LabResult] [Field:PAR] [Status:valid] Value={round}")
             return str(round)
         except (InvalidOperation, ValueError):
-            logger.warning(f"[LabResult] [Field:PAR] [Status:invalid] Value={value}")
+            # logger.warning(f"[LabResult] [Field:PAR] [Status:invalid] Value={value}")
             return "Not Tested"
 
     def model_post_init(self, __context):
         """بعد از ساخت مدل، فیلدهای محاسباتی را مقداردهی کن"""
 
-        logger.info("[LabResult] [PostInit] Starting post-initialization...")
+        # logger.info("[LabResult] [PostInit] Starting post-initialization...")
         # timestamp از تاریخ شمسی ساخته می‌شود
         hour = int(self.time[:2])
         minute = int(self.time[2:])
@@ -91,9 +93,9 @@ class LabResult(BaseModel):
         base_date = jdatetime.date(year_int, month_int, day_int)
         if 0 <= hour < 8:
             base_date = base_date + jdatetime.timedelta(days=1)
-            logger.info(
-                f"[LabResult] [PostInit] Adjusted date forward due to early hour: {base_date}"
-            )
+            # logger.info(
+            #     f"[LabResult] [PostInit] Adjusted date forward due to early hour: {base_date}"
+            # )
 
         # همگام‌سازی year/month/day با تاریخ جدید
         self.year = f"{base_date.year:04d}"
@@ -111,27 +113,29 @@ class LabResult(BaseModel):
             .togregorian()
             .timestamp()
         )
-        logger.debug(f"[LabResult] [PostInit] Timestamp generated: {self.timestamp}")
+        # logger.debug(f"[LabResult] [PostInit] Timestamp generated: {self.timestamp}")
 
         # par10 = par05 + par51
         if self.par05 != "Not Tested" and self.par51 != "Not Tested":
             self.par10 = str(Decimal(self.par05) + Decimal(self.par51))
-            logger.debug(f"[LabResult] [PostInit] par10 calculated: {self.par10}")
+            # logger.debug(f"[LabResult] [PostInit] par10 calculated: {self.par10}")
 
         else:
             self.par10 = "Not Tested"
-            logger.warning("[LabResult] [PostInit] par10 not calculated due to missing values")
+            # logger.warning("[LabResult] [PostInit] par10 not calculated due to missing values")
 
         # par16 = 100 - par10 - par60
         if self.par10 != "Not Tested" and self.par60 != "Not Tested":
-            self.par16 = str(Decimal("100.0") - Decimal(self.par10) - Decimal(self.par60))
-            logger.debug(f"[LabResult] [PostInit] par16 calculated: {self.par16}")
+            self.par16 = str(
+                Decimal("100.0") - Decimal(self.par10) - Decimal(self.par60)
+            )
+            # logger.debug(f"[LabResult] [PostInit] par16 calculated: {self.par16}")
 
         else:
             self.par16 = "Not Tested"
-            logger.warning("[LabResult] [PostInit] par16 not calculated due to missing values")
+            # logger.warning("[LabResult] [PostInit] par16 not calculated due to missing values")
 
-        logger.info("[LabResult] [PostInit] Completed successfully.")
+        # logger.info("[LabResult] [PostInit] Completed successfully.")
 
 
 # Example :
